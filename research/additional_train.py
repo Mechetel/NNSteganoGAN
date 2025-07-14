b464d41a37d88d1534852878c55a7205f6712336
Mechetel <dima.homa5@gmail.com>
2025-07-15 01:51:02 +0300

    feat: train wgan_gp + additional train wgan

--- research/additional_train.py
+++ research/additional_train.py
@@ -1,46 +1,33 @@
 #!/usr/bin/env python3
 
-import json
-import os
-
-import torch
-
 import sys, os
 sys.path.append('..')
 
-from steganogan.models import SteganoGAN
-from steganogan.critics import BasicCritic
-from steganogan.decoders import BasicDecoder, DenseDecoder
-from steganogan.encoders import BasicEncoder, ResidualEncoder, DenseEncoder
-from steganogan.loader import DataLoader
+import torch
+
+from steganogan_wgan_gp.models import SteganoGAN
+from steganogan_wgan_gp.critics import BasicCritic
+from steganogan_wgan_gp.decoders import BasicDecoder, DenseDecoder
+from steganogan_wgan_gp.encoders import BasicEncoder, ResidualEncoder, DenseEncoder
+from steganogan_wgan_gp.loader import DataLoader
 
 
 def main():
     torch.manual_seed(42)
-    training_type = 'original'
-    timestamp = '1751999939'
+    training_type = 'wgan'
+    timestamp = '1752271900'
 
     train = DataLoader(os.path.join("data", "div2k", "train"), shuffle=True)
     validation = DataLoader(os.path.join("data", "div2k", "val"), shuffle=False)
 
-    steganogan = SteganoGAN(
-        data_depth=1,
-        encoder=DenseEncoder,
-        decoder=DenseDecoder,
-        critic=BasicCritic,
+    steganogan = SteganoGAN.load(
+        path=os.path.join("models", training_type, timestamp, "20.rsbpp-0.964984.p"),
         cuda=True,
         verbose=True,
         log_dir=os.path.join('models', training_type, timestamp)
     )
-    with open(os.path.join("models", training_type, timestamp, "config.json"), "wt") as fout:
-        fout.write(json.dumps({
-            "epochs": 32,
-            "encoder": "dense",
-            "data_depth": 1,
-            "dataset": "div2k",
-        }, indent=2, default=lambda o: str(o)))
 
-    steganogan.fit(train, validation, epochs=32)
+    steganogan.fit(train, validation, epochs=13, start_epoch=21)
     steganogan.save(os.path.join("models", training_type, timestamp, "weights.steg"))
 
 if __name__ == '__main__':
